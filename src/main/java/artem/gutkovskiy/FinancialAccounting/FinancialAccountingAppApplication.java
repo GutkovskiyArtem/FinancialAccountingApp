a51343f3fd0ba5362904689c7d8f45259b5247f9
Artem <artem@dial-up.86.57.213.42.grodno.by>
2024-04-01 11:34:04 +0300

    Initial commit

---
+++ src/main/java/artem/gutkovskiy/FinancialAccounting/FinancialAccountingAppApplication.java
@@ -0,0 +1,13 @@
+package artem.gutkovskiy.FinancialAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAccountingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAccountingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artem/gutkovskiy/FinancialAccounting/FinancialAccountingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package artem.gutkovskiy.FinancialAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAccountingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
