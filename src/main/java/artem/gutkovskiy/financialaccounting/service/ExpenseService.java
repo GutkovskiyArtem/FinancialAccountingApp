5540ad46a8929a864cbdeecc997d26efc2b13499
Gutkovskiy Artem <gutart0404@gmail.com>
2024-09-04 10:04:51 +0300

    added endpoint to search expenses by username. added cache

--- src/main/java/artem/gutkovskiy/financialaccounting/service/ExpenseService.java
+++ src/main/java/artem/gutkovskiy/financialaccounting/service/ExpenseService.java
@@ -1,32 +1,53 @@
-package artem.gutkovskiy.financialaccounting.service;
-import artem.gutkovskiy.financialaccounting.entity.Expense;
-import artem.gutkovskiy.financialaccounting.repository.ExpenseRepository;
-import org.springframework.stereotype.Service;
+    package artem.gutkovskiy.financialaccounting.service;
 
-import java.util.List;
-import java.util.Optional;
+    import artem.gutkovskiy.financialaccounting.entity.Expense;
+    import artem.gutkovskiy.financialaccounting.repository.ExpenseRepository;
+    import artem.gutkovskiy.financialaccounting.cache.ExpenseCache;
+    import org.springframework.stereotype.Service;
 
-@Service
-public class ExpenseService {
-    private final ExpenseRepository expenseRepository;
+    import java.util.List;
+    import java.util.Optional;
 
-    public ExpenseService(ExpenseRepository expenseRepository) {
-        this.expenseRepository = expenseRepository;
+    @Service
+    public class ExpenseService {
+        private final ExpenseRepository expenseRepository;
+        private final ExpenseCache expenseCache;
+
+        public ExpenseService(ExpenseRepository expenseRepository, ExpenseCache expenseCache) {
+            this.expenseRepository = expenseRepository;
+            this.expenseCache = expenseCache;
+        }
+
+        public List<Expense> findAll() {
+            return expenseRepository.findAll();
+        }
+
+        public Optional<Expense> findById(Long id) {
+            return expenseRepository.findById(id);
+        }
+
+        public Expense save(Expense expense) {
+            expenseCache.invalidate(expense.getUserName());
+            return expenseRepository.save(expense);
+        }
+
+        public void deleteById(Long id) {
+            expenseRepository.findById(id).ifPresent(expense -> {
+                expenseCache.invalidate(expense.getUserName());
+                expenseRepository.deleteById(id);
+            });
+        }
+
+        public List<Expense> findExpensesByUserName(String userName) {
+            if (expenseCache.containsKey(userName)) {
+                return expenseCache.get(userName);
+            } else {
+                List<Expense> expenses = expenseRepository.findExpensesByUserName(userName);
+                if (!expenses.isEmpty()) {
+                    expenseCache.put(userName, expenses);
+                }
+
+                return expenses;
+            }
+        }
     }
-
-    public List<Expense> findAll() {
-        return expenseRepository.findAll();
-    }
-
-    public Optional<Expense> findById(Long id) {
-        return expenseRepository.findById(id);
-    }
-
-    public Expense save(Expense expense) {
-        return expenseRepository.save(expense);
-    }
-
-    public void deleteById(Long id) {
-        expenseRepository.deleteById(id);
-    }
-}
